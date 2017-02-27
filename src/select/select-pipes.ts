f1558f96828a99c207f55facfa501bb1ba982563
IlyaAndr <aimilandr@gmail.com>
2017-02-27 12:35:15 +0300

    Hack highlight pipe

--- src/select/select-pipes.ts
+++ src/select/select-pipes.ts
@@ -4,6 +4,10 @@ import { escapeRegexp } from './common';
 @Pipe({name: 'highlight'})
 export class HighlightPipe implements PipeTransform {
   public transform(value:string, query:string):any {
+    // HACK
+    // Wait when https://github.com/valor-software/ng2-select/issues/633 is fixed
+    return value;
+
     if (query.length < 1) {
       return value;
     }
